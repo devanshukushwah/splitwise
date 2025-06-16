04465334947d6544788020a895a09f1780f71e57
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-06-17 01:04:47 +0530

    added tab for spend report

--- src/components/SpendResult.js
+++ src/components/SpendResult.js
@@ -1,5 +1,33 @@
 import { Divider, List, ListItem, ListItemText } from "@mui/material";
 import React from "react";
+import { Tabs, Tab, Box, Typography } from "@mui/material";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const style = {
   py: 0,
@@ -11,16 +39,14 @@ const style = {
   backgroundColor: "background.paper",
 };
 
-function SpendResult({ data, people }) {
-  // group name based on person_id
-  let peopleMap = {};
-  for (let obj of people) {
-    peopleMap[obj.person_id] = obj.personName;
+function ShowResultBox({ list, peopleMap }) {
+  if (!list || list.length === 0) {
+    return <Typography>No transactions to show</Typography>;
   }
 
   return (
     <List sx={style}>
-      {data?.transactions?.map((spend, index) => (
+      {list?.map((spend, index) => (
         <React.Fragment key={index}>
           <ListItem>
             <ListItemText
@@ -33,6 +59,42 @@ function SpendResult({ data, people }) {
         </React.Fragment>
       ))}
     </List>
+  );
+}
+
+function SpendResult({ data, people }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  // group name based on person_id
+  let peopleMap = {};
+  for (let obj of people) {
+    peopleMap[obj.person_id] = obj.personName;
+  }
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Final Transactions" {...a11yProps(0)} />
+        <Tab label="All Transactions" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <ShowResultBox
+          list={data.optimizedTransactions || []}
+          peopleMap={peopleMap}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <ShowResultBox list={data.transactions || []} peopleMap={peopleMap} />
+      </TabPanel>
+    </Box>
   );
 }
 
