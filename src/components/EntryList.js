37c516c243d37b8c45f4322ac1392400a3db54c3
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-07-10 12:41:26 +0530

    add loading for dashboard page.

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -13,14 +13,67 @@ import {
   Typography,
   Stack,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EastIcon from "@mui/icons-material/East";
 import { useRouter } from "next/navigation";
+import api from "@/lib/axios";
+import { HttpUrlConfig } from "@/core/HttpUrlConfig";
+import Loader from "./Loader";
 
-export default function EntryList({ entries = [], onAdd }) {
+export default function EntryList() {
   const [open, setOpen] = useState(false);
   const [entryName, setEntryName] = useState("");
-  const router = useRouter();
+  const [entries, setEntries] = useState([]);
+  const [entriesLoading, setEntriesLoading] = useState(true);
+  const [addEntryLoading, setAddEntryLoading] = useState(false);
+
+  const stopEntriesLoading = () => {
+    setEntriesLoading(false);
+  };
+
+  const startEntriesLoading = () => {
+    setEntriesLoading(true);
+  };
+
+  const startAddEntryLoading = () => {
+    setAddEntryLoading(true);
+  };
+
+  const stopAddEntryLoading = () => {
+    setAddEntryLoading(false);
+    handleClose();
+  };
+
+  const handleAddEntry = () => {
+    if (entryName.trim() === "") return;
+    startAddEntryLoading();
+    api
+      .post(HttpUrlConfig.postEntryUrl(), { title: entryName.trim() })
+      .then((response) => {
+        setEntries([{ ...entry, _id: response?.data?.entry_id }, ...entries]);
+        stopAddEntryLoading();
+      })
+      .catch((error) => {
+        console.error("Error adding entry:", error);
+        stopAddEntryLoading();
+      });
+  };
+
+  const fetchEntries = async () => {
+    startEntriesLoading();
+    try {
+      const response = await api.get(HttpUrlConfig.getEntriesUrl());
+      setEntries(response.data.entries);
+      stopEntriesLoading();
+    } catch (error) {
+      console.error("Error fetching entries:", error);
+      stopEntriesLoading();
+    }
+  };
+
+  useEffect(() => {
+    fetchEntries();
+  }, []);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
@@ -28,84 +81,92 @@ export default function EntryList({ entries = [], onAdd }) {
     setOpen(false);
   };
 
-  const handleSubmit = () => {
-    if (entryName.trim() === "") return;
-    onAdd({ title: entryName.trim() });
-    handleClose();
-  };
+  return (
+    <>
+      <Box sx={{ mt: 4 }}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          mb={2}
+        >
+          <Typography variant="h5">Entries</Typography>
+          <Button variant="contained" color="primary" onClick={handleOpen}>
+            Add Entry
+          </Button>
+        </Box>
+
+        {entriesLoading ? <Loader /> : <Entries entries={entries} />}
+
+        {/* Dialog */}
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Add New Entry</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Entry Name"
+              fullWidth
+              value={entryName}
+              onChange={(e) => setEntryName(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="secondary">
+              Cancel
+            </Button>
+            <Button
+              onClick={handleAddEntry}
+              variant="contained"
+              color="primary"
+              loading={addEntryLoading}
+            >
+              Add
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </>
+  );
+}
+
+const Entries = ({ entries }) => {
+  const router = useRouter();
 
   const handleGoClick = (entryId) => {
     if (!entryId) return;
     router.push(`/entry/${entryId}`);
   };
-
   return (
-    <Box sx={{ mt: 4 }}>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        mb={2}
-      >
-        <Typography variant="h5">Entries</Typography>
-        <Button variant="contained" color="primary" onClick={handleOpen}>
-          Add Entry
-        </Button>
-      </Box>
-
-      <Stack spacing={2}>
-        {entries.length === 0 ? (
-          <Typography variant="body1" color="text.secondary">
-            No entries yet.
-          </Typography>
-        ) : (
-          entries.map((entry, index) => (
-            <Card key={index} variant="outlined">
-              <CardContent>
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  alignItems="center"
-                  justifyContent="space-between"
+    <Stack spacing={2}>
+      {entries.length === 0 ? (
+        <Typography variant="body1" color="text.secondary">
+          No entries yet.
+        </Typography>
+      ) : (
+        entries.map((entry, index) => (
+          <Card key={index} variant="outlined">
+            <CardContent>
+              <Stack
+                direction="row"
+                spacing={1}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Typography variant="body1">{entry.title}</Typography>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  startIcon={<EastIcon />}
+                  onClick={() => handleGoClick(entry._id)}
                 >
-                  <Typography variant="body1">{entry.title}</Typography>
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    startIcon={<EastIcon />}
-                    onClick={() => handleGoClick(entry._id)}
-                  >
-                    Go
-                  </Button>
-                </Stack>
-              </CardContent>
-            </Card>
-          ))
-        )}
-      </Stack>
-
-      {/* Dialog */}
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add New Entry</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Entry Name"
-            fullWidth
-            value={entryName}
-            onChange={(e) => setEntryName(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="secondary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} variant="contained" color="primary">
-            Add
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Box>
+                  Go
+                </Button>
+              </Stack>
+            </CardContent>
+          </Card>
+        ))
+      )}
+    </Stack>
   );
-}
+};
